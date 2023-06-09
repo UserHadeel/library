8bee9107e12f8c583b327bc64397396b3d9b3a22
allaghi <allaghi@yahoo.com>
2023-06-09 07:35:33 +0200

    Update 2023_06_08_165548_create_books_table.php
    adding auther_id as foreign id

--- database/migrations/2023_06_08_165548_create_books_table.php
+++ database/migrations/2023_06_08_165548_create_books_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('books', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->string('author');
+            $table->foreignId('auther_id')->nullable()->constrained()->onDelete('cascade');
             $table->string('isbn');
             $table->text('description')->nullable();
             $table->unsignedBigInteger('category_id');
