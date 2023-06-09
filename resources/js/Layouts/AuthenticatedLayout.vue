bf13eb22b5b4293643d7ca3fa6792006de820586
allaghi <allaghi@yahoo.com>
2023-06-09 21:17:34 +0200

    Update AuthenticatedLayout.vue
    Updating AuthenticatedLayout

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -65,9 +65,9 @@ const showingNavigationDropdown = ref(false);
                                     </template>
 
                                     <template #content>
-                                        <DropdownLink :href="route('profile.edit')"> Profile </DropdownLink>
+                                        <DropdownLink :href="route('profile.edit')"> حسابي </DropdownLink>
                                         <DropdownLink :href="route('logout')" method="post" as="button">
-                                            Log Out
+                                            خروج
                                         </DropdownLink>
                                     </template>
                                 </Dropdown>
@@ -128,9 +128,9 @@ const showingNavigationDropdown = ref(false);
                         </div>
 
                         <div class="mt-3 space-y-1">
-                            <ResponsiveNavLink :href="route('profile.edit')"> Profile </ResponsiveNavLink>
+                            <ResponsiveNavLink :href="route('profile.edit')"> حسابي </ResponsiveNavLink>
                             <ResponsiveNavLink :href="route('logout')" method="post" as="button">
-                                Log Out
+                                الخروج
                             </ResponsiveNavLink>
                         </div>
                     </div>
